cd97112e9b6a9b2bfe5d0d064c03ab726dc68231
Mike Gering <mike@mfg-consulting-llc.com>
2024-01-10 17:38:50 -0500

    x

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,17 +1,17 @@
 import sys, os
 cwd = os.getcwd()
 sys.path.append(cwd)
-sys.path.append(cwd + '/morgan_ai')
+sys.path.append(cwd + '/morgan_proj')
 
-INTERP = os.path.expanduser("~/django.ferree-gering.com/.venv/bin/python")
+INTERP = os.path.expanduser("~/morgan-ai.ferree-gering.com/.venv/bin/python")
 
 if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
 
-sys.path.insert(0,'/home/ferreegering/django.ferree-gering.com/.venv/bin')
-#sys.path.insert(0,'/home/ferreegering/django.ferree-gering.com/.venv/lib/python3.9/site-packages/django')
-sys.path.insert(0,'/home/ferreegering/django.ferree-gering.com/.venv/lib/python3.9/site-packages')
+sys.path.insert(0,'/home/ferreegering/morgan-ai.ferree-gering.com/.venv/bin')
+#sys.path.insert(0,'/home/ferreegering/morgan-ai.ferree-gering.com/.venv/lib/python3.9/site-packages/django')
+sys.path.insert(0,'/home/ferreegering/morgan-ai.ferree-gering.com/.venv/lib/python3.9/site-packages')
 
-os.environ['DJANGO_SETTINGS_MODULE'] = "morgan_ai.settings"
+os.environ['DJANGO_SETTINGS_MODULE'] = "morgan_proj.settings"
 from morgan_proj.wsgi import application
 #from django.core.wsgi import get_wsgi_application
 #application = get_wsgi_application()
